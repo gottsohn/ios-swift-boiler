941db097ab2f335a432947a9887f33d085dab1bf
andela-gukpere <godson.ukpere@andela.co>
2016-03-13 23:22:55 +0100

    Initial Commit

---
+++ ios swift boilerTests/ios_swift_boilerTests.swift
@@ -0,0 +1,36 @@
+//
+//  ios_swift_boilerTests.swift
+//  ios swift boilerTests
+//
+//  Created by Godson Ukpere on 3/13/16.
+//  Copyright © 2016 Godson Ukpere. All rights reserved.
+//
+
+import XCTest
+@testable import ios_swift_boiler
+
+class ios_swift_boilerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}

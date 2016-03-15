c640af7be492d40f291c2c87f70b839d78eb37b8
andela-gukpere <godson.ukpere@andela.co>
2016-03-15 20:04:21 +0100

    refactor(Users model): Add fetch and delete functions to User Model

--- ios swift boiler/Users.swift
+++ ios swift boiler/Users.swift
@@ -8,8 +8,8 @@
 
 import CoreData
 import SwiftyJSON
-
 @objc(Users)
+
 class Users: NSManagedObject {
     @NSManaged var name: String
     @NSManaged var platform: String
@@ -46,5 +46,74 @@ class Users: NSManagedObject {
             callback(context: context, result: nil, error: error)
         }
     }
-
+    
+    static func managedObject (callback:(context:NSManagedObjectContext, user:Users)->()) {
+        let appDelegate: AppDelegate = (UIApplication.sharedApplication().delegate as! AppDelegate)
+        let context: NSManagedObjectContext = appDelegate.managedObjectContext
+        let en = NSEntityDescription.entityForName(Const.MODEL_USERS, inManagedObjectContext: context)
+        callback(context: context, user: Users(entity: en!, insertIntoManagedObjectContext: context))
+    }
+    
+    static func saveUser (view:UIViewController? = nil, authUser:JSON, token:String? = nil, callback: (error: NSError?)->()) {
+        Helpers.currentUser = authUser;
+        fetch(Const.MODEL_USERS) { context, result, error in
+            if error == nil && result != nil {
+                let user = result!.first
+                if authUser[Const.KEY_OAUTH_TOKEN].isExists() {
+                    user?.setValue(authUser[Const.KEY_OAUTH_TOKEN].stringValue, forKey: Const.KEY_OAUTH_TOKEN)
+                }
+                
+                if authUser[Const.KEY_OAUTH_TOKEN_SECRET].isExists() {
+                    user?.setValue(authUser[Const.KEY_OAUTH_TOKEN_SECRET].stringValue, forKey: Const.KEY_OAUTH_TOKEN_SECRET)
+                }
+                
+                user?.setValue(authUser[Const.KEY_USERNAME].stringValue, forKey: Const.KEY_USERNAME)
+                user?.setValue(authUser[Const.KEY_NAME].stringValue, forKey: Const.KEY_NAME)
+                user?.setValue(authUser[Const.KEY_EMAIL].stringValue, forKey: Const.KEY_EMAIL)
+                user?.setValue(authUser[Const.KEY_DESCRIPTION].stringValue, forKey: Const.KEY_DESCRIPTION)
+                Helpers.saveManagedContext(view!, context: context, callback: callback)
+            } else {
+                managedObject({ context, user in
+                    user.name = authUser[Const.KEY_NAME].stringValue
+                    user.desc = authUser[Const.KEY_DESCRIPTION].stringValue
+                    user.oauth_token = authUser[Const.KEY_OAUTH_TOKEN].stringValue
+                    user.oauth_token_secret = authUser[Const.KEY_OAUTH_TOKEN_SECRET].stringValue
+                    user.email = authUser[Const.KEY_EMAIL].stringValue
+                    user.user_id = authUser[Const.KEY_ID].stringValue
+                    user.username = authUser[Const.KEY_USERNAME].stringValue
+                    user.img = authUser[Const.KEY_IMG].stringValue
+                    user.bg_img = authUser[Const.KEY_BG_IMG].stringValue
+                    user.platform = authUser[Const.KEY_PLATFORM].stringValue
+                    Helpers.saveManagedContext(view, context: context, callback: callback)
+                })
+            }
+        }
+    }
+    
+    static func deleteData (limit:Int = 0, callback: ((count:Int, error:NSError?) -> ())? = nil) {
+        fetch(Const.MODEL_USERS, sortBy: "user_id", sortAsc: true) { (context, result, error) -> () in
+            var count:Int = 0
+            if result != nil {
+                for managedObject in result! {
+                    if count == limit && limit != 0 {
+                        break
+                    }
+                    
+                    context.deleteObject(managedObject)
+                    count++
+                }
+                
+                Helpers.saveManagedContext(context: context) {
+                    errror in
+                    if callback != nil {
+                        callback!(count: count,error: error)
+                    }
+                }
+            }
+            
+            if callback != nil {
+                callback!(count: count, error: nil)
+            }
+        }
+    }
 }

1d5d6e802c7013c65be353acc69365d11db0175b
Godson Ukpere <godson.ukpere@gmail.com>
2016-08-18 15:32:22 +0100

    chore(Pods): Remove Alamofire framework

--- Pods/Target Support Files/Pods-IOSSwiftBoilerTests/Pods-IOSSwiftBoilerTests-frameworks.sh
+++ Pods/Target Support Files/Pods-IOSSwiftBoilerTests/Pods-IOSSwiftBoilerTests-frameworks.sh
@@ -84,10 +84,8 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "Pods-IOSSwiftBoilerTests/Alamofire.framework"
   install_framework "Pods-IOSSwiftBoilerTests/SwiftyJSON.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "Pods-IOSSwiftBoilerTests/Alamofire.framework"
   install_framework "Pods-IOSSwiftBoilerTests/SwiftyJSON.framework"
 fi
--- Pods/Target Support Files/Pods-IOSSwiftBoilerUITests/Pods-IOSSwiftBoilerUITests-frameworks.sh
+++ Pods/Target Support Files/Pods-IOSSwiftBoilerUITests/Pods-IOSSwiftBoilerUITests-frameworks.sh
@@ -84,10 +84,8 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "Pods-IOSSwiftBoilerUITests/Alamofire.framework"
   install_framework "Pods-IOSSwiftBoilerUITests/SwiftyJSON.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "Pods-IOSSwiftBoilerUITests/Alamofire.framework"
   install_framework "Pods-IOSSwiftBoilerUITests/SwiftyJSON.framework"
 fi
